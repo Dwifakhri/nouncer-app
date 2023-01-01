559c6bbf70867bb929983c5576a538160f033524
dwifakhri <fakhri.chusaini01@gmail.com>
2023-01-02 02:30:17 +0700

    favorites and saved word

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,10 +1,91 @@
 import React from "react";
+import { AiTwotoneSound, AiFillStar } from "react-icons/ai";
 
-const Box = () => {
+const Box = ({ data, handleFavorites, params }) => {
   return (
-    <div>
-      <p>Result of Hello</p>
-    </div>
+    <>
+      {data?.length === 0 || !data ? (
+        ""
+      ) : data ? (
+        <div className="mt-5">
+          <div className="flex space-x-10">
+            <p className="text-2xl">
+              Results of <span className="font-semibold">"{data?.word}"</span>
+            </p>
+            {!params && (
+              <div
+                onClick={handleFavorites}
+                className="flex flex-row space-x-4 text-blue-500 items-center cursor-pointer"
+              >
+                <p>Add to Favorites</p>
+                <AiFillStar size={25} />
+              </div>
+            )}
+          </div>
+
+          <div>
+            <div className="flex space-x-5 items-center my-3">
+              <div className="rounded-full shadow-lg shadow-gray-500 p-4">
+                <AiTwotoneSound size={30} />
+              </div>
+              <div className="flex flex-col space-y-2">
+                <p className="font-semibold text-xl">{data?.word}</p>
+                <p>{data?.phonetics[0]?.text}</p>
+              </div>
+            </div>
+            <div className="flex flex-col my-10">
+              <p className="font-semibold text-lg">Meaning</p>
+              {data?.meanings?.map((item, index) => (
+                <div
+                  key={index}
+                  className="flex flex-col space-y-3 mb-2 shadow-lg p-5"
+                >
+                  <div className="flex space-x-10">
+                    <p>Part of</p>
+                    <p className="font-semibold">: {item.partOfSpeech}</p>
+                  </div>
+                  <div className="flex space-x-5">
+                    <p>Definition</p>
+                    <div className="flex flex-col space-y-3">
+                      {item?.definitions?.map((item, index) => (
+                        <div key={index}>
+                          <p>: {item.definition}</p>
+                          {item.example && <p> ex: {item.example}</p>}
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                  <div className="flex space-x-5">
+                    <p>Synonym</p>
+                    {item.synonyms && (
+                      <p>: {item.synonyms.map((item) => item + ", ")}</p>
+                    )}
+                  </div>
+                  <div className="flex space-x-6">
+                    <p>Antonym</p>
+                    {item.antonyms && (
+                      <p>: {item.antonyms.map((item) => item + ", ")}</p>
+                    )}
+                  </div>
+                  <div className="flex space-x-9">
+                    <p>Source</p>
+                    <a
+                      href={data?.sourceUrls}
+                      target="blank"
+                      rel="noopener noreferrer"
+                    >
+                      : {data?.sourceUrls}
+                    </a>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      ) : (
+        <p className="text-center">No Data</p>
+      )}
+    </>
   );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
+import Box from "../components/Box";
 import Layout from "../components/Layout";
-import { AiTwotoneSound } from "react-icons/ai";
 import { BsSearch } from "react-icons/bs";
+import { useDispatch } from "react-redux";
+import { setFavorites } from "../utils/redux/reducers/reducers";
 
 const home = () => {
+  const dispatch = useDispatch();
   const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(false);
+  // const [loading, setLoading] = useState(false);
   const [keyword, setKeyword] = useState("");
 
   const searchWord = async (e) => {
@@ -16,9 +19,31 @@ const home = () => {
       );
       const results = await response.json();
       setData(results[0]);
-      console.log(results[0]);
     } catch {
-      alert("error");
+      alert("Field is required");
+    }
+  };
+  const handleFavorites = async (data) => {
+    const myLocalWord = localStorage.getItem("favWords");
+    if (myLocalWord) {
+      const objWord = JSON.parse(myLocalWord);
+      const sameWord = objWord.find((item) => item.word === data.word);
+      if (sameWord) {
+        const tempWord = objWord.filter((item) => item.word !== data.word);
+        objWord.pop(tempWord);
+        alert("Already in favorites");
+      } else {
+        objWord.push(data);
+        alert("Success");
+        const storageWord = JSON.stringify(objWord);
+        dispatch(setFavorites(objWord));
+        localStorage.setItem("favWords", storageWord);
+      }
+    } else {
+      const storageWord = JSON.stringify([data]);
+      dispatch(setFavorites([data]));
+      alert("Success to add");
+      localStorage.setItem("favWords", storageWord);
     }
   };
 
@@ -41,57 +66,9 @@ const home = () => {
             required
           />
 
-          <BsSearch />
+          <BsSearch className="cursor-pointer" onClick={searchWord} />
         </form>
-        {!data || data.length === 0 ? (
-          <p className="text-center mt-3">No Data</p>
-        ) : (
-          <div className="mt-5">
-            <p className="text-2xl">
-              Results of <span className="font-semibold">{data.word}</span>
-            </p>
-
-            <div>
-              <div className="flex space-x-5 items-center my-3">
-                <div className="rounded-full shadow-lg shadow-gray-500 p-4">
-                  <AiTwotoneSound size={30} />
-                </div>
-                <div className="flex flex-col space-y-2">
-                  <p className="font-semibold text-xl">{data.word}</p>
-                  <p>{data.phonetics[0]?.text}</p>
-                </div>
-              </div>
-              <div className="flex flex-col my-10">
-                <p className="font-semibold text-lg">Meaning</p>
-                {data?.meanings?.map((item, index) => (
-                  <div key={index} className="flex flex-col space-y-3 mb-2">
-                    <div className="flex space-x-10">
-                      <p>Part of</p>
-                      <p className="font-semibold">: {item.partOfSpeech}</p>
-                    </div>
-                    <div className="flex space-x-5">
-                      <p>Definition</p>
-                      <p>{item.defini}</p>
-                    </div>
-                    <div className="flex space-x-5">
-                      <p>Synonym</p>
-                      <p>: No</p>
-                    </div>
-                    <div className="flex space-x-6">
-                      <p>Antonym</p>
-                      <p>: No</p>
-                    </div>
-                    <div className="flex space-x-9">
-                      <p>Source</p>
-                      <a href="">: Link URL</a>
-                    </div>
-                    <hr />
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div>
-        )}
+        <Box data={data} handleFavorites={() => handleFavorites(data)} />
       </div>
     </Layout>
   );
